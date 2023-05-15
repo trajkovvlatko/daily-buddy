51ccdcd639a3a0fb98f4bae4c4b2df81ac69e2df
Vlatko Trajkov <trajkovvlatko@gmail.com>
2023-05-15 21:14:41 +0200

    Update menus

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,35 +1,34 @@
 import { Link, routes } from '@redwoodjs/router';
 import ProfileMenu from '../ProfileMenu/ProfileMenu';
+import { BookOpenIcon, CalendarDaysIcon, HomeIcon, PencilSquareIcon } from '@heroicons/react/24/solid';
 
 const Header = () => {
   return (
-    <header className="mb-2 h-16 w-full shadow-lg">
-      <div className="h-full items-center overflow-auto">
-        <ul className="flex  h-full items-center">
-          <li className="ml-6 mr-6">
-            <Link to={routes.dashboard()} className="text-blue-500 hover:text-blue-800">
-              Dashboard
-            </Link>
-          </li>
-          <li className="mr-6">
-            <Link to={routes.notes()} className="text-blue-500 hover:text-blue-800">
-              Notes
-            </Link>
-          </li>
-          <li className="mr-6">
-            <Link to={routes.journals()} className="text-blue-500 hover:text-blue-800">
-              Journal
-            </Link>
-          </li>
-          <li className="mr-6">
-            <Link to={routes.inventory()} className="text-blue-500 hover:text-blue-800">
-              Inventory
-            </Link>
-          </li>
-          <ProfileMenu />
-        </ul>
-      </div>
-    </header>
+    <ul className="flex h-16 items-center justify-around md:h-full md:flex-col md:justify-between">
+      <li className="md:mb-8 md:mt-6">
+        <Link to={routes.dashboard()} className="text-blue-500 hover:text-blue-800">
+          <CalendarDaysIcon className="h-6 w-6 text-blue-500" />
+        </Link>
+      </li>
+      <li className="md:mb-8">
+        <Link to={routes.notes()} className="text-blue-500 hover:text-blue-800">
+          <PencilSquareIcon className="h-6 w-6 text-blue-500" />
+        </Link>
+      </li>
+      <li className="md:mb-8">
+        <Link to={routes.journals()} className="text-blue-500 hover:text-blue-800">
+          <BookOpenIcon className="h-6 w-6 text-blue-500" />
+        </Link>
+      </li>
+      <li className="md:mb-8">
+        <Link to={routes.inventory()} className="text-blue-500 hover:text-blue-800">
+          <HomeIcon className="h-6 w-6 text-blue-500" />
+        </Link>
+      </li>
+      <li className="md:mt-auto md:mb-3">
+        <ProfileMenu />
+      </li>
+    </ul>
   );
 };
 
