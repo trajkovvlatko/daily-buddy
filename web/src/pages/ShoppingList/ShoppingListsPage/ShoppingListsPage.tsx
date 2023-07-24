32a46d6396c6a8ff243be104eb7616bb28e33cdb
Vlatko Trajkov <trajkovvlatko@gmail.com>
2023-07-25 00:35:53 +0200

    Clean up

--- web/src/pages/ShoppingList/ShoppingListsPage/ShoppingListsPage.tsx
+++ web/src/pages/ShoppingList/ShoppingListsPage/ShoppingListsPage.tsx
@@ -5,7 +5,7 @@ import ShoppingListsCell from 'src/components/ShoppingList/ShoppingListsCell';
 const ShoppingListsPage = () => {
   return <PageWrapper>
     <div className='col-span-12 p-6'>
-      <Link to={routes.newShoppingList()} className="rw-button rw-button-green float-right mb-6 w-64">
+      <Link to={routes.newShoppingList()} className="green-button flex float-right mb-6 pl-6 pr-6">
         <div className="rw-button-icon">+</div> New shopping list
       </Link>
       <ShoppingListsCell />
